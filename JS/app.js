3f1eb4c61c7ff47315d9a2c79176d8ab6490caea
Ruban Gino Singh <rubangino22@gmail.com>
2023-05-26 17:48:08 +0530

    ADD api secrets

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,8 @@
 // Getting Openweathermap API Key From the config.js file
 var API_KEY = config.KEY; 
 
+var apiKey = process.env.api_key;
+
 const wrapper = document.querySelector(".wrapper"),
     inputPart = wrapper.querySelector(".inputs"),
     infoTxt = inputPart.querySelector(".info-txt"),
